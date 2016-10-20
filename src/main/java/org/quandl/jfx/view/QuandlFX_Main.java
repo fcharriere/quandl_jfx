6ae60c7db2fca850805fd23fdf0fabb335566d2e
frederic <frederic@algopro.io>
2016-10-20 20:50:11 +0200

    --

--- src/main/java/org/quandl/jfx/view/QuandlFX_Main.java
+++ src/main/java/org/quandl/jfx/view/QuandlFX_Main.java
@@ -33,14 +33,17 @@ import org.quandl.jfx.view.wiki.tasks.CreateWIKIDBTask;
  * @author frederic
  */
 public class QuandlFX_Main extends Application {
-
+    
     private final TableView<DataSetFX> table = new TableView<>();
     private final TabPane tabPane = new TabPane();
     private final ObservableList<DataSetFX> data = FXCollections.observableArrayList();
-
+    
+    
     @Override
     public void start(Stage primaryStage) {
-
+        
+        setUserAgentStylesheet(STYLESHEET_CASPIAN);
+        
         DataSetTableChangeListener listener = new DataSetTableChangeListener(table, tabPane);
         table.getSelectionModel().selectedItemProperty().addListener(listener);
         table.setEditable(true);
@@ -49,10 +52,12 @@ public class QuandlFX_Main extends Application {
 //        MENU
         MenuBar menuBar = new MenuBar();
         Menu menuFile = new Menu("File");
+        Menu menuTabs = new Menu("Tabs");
         MenuItem menuItemExit = new MenuItem("Exit", new ImageView(new Image("icons/ic_exit_to_app_black_18dp.png", 14, 14, false, false)));
         menuFile.getItems().add(menuItemExit);
-        menuBar.getMenus().add(menuFile);
-
+        menuBar.getMenus().addAll(menuFile,menuTabs);
+        
+        
         menuItemExit.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -60,69 +65,77 @@ public class QuandlFX_Main extends Application {
                 System.exit(0);
             }
         });
+        
+        menuTabs.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                
+            }
+        });
 
 //        LEFT
         TableColumn<DataSetFX, String> codeColumn = new TableColumn<DataSetFX, String>("Code");
         codeColumn.setCellValueFactory(new PropertyValueFactory<>("code"));
-
+        
         TableColumn descriptionColumn = new TableColumn("Description");
         descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
-
+        
         table.setColumnResizePolicy(TableView.UNCONSTRAINED_RESIZE_POLICY);
         table.getColumns().addAll(codeColumn, descriptionColumn);
         table.setItems(data);
-
+        
         VBox vBox = new VBox();
         vBox.setSpacing(5);
         vBox.setPadding(new Insets(1, 1, 1, 1));
         VBox.setVgrow(table, Priority.ALWAYS);
         vBox.getChildren().addAll(table);
         vBox.setStyle("-fx-background-color: #336699;");
-        vBox.setPrefWidth(350D);
+        vBox.setPrefWidth(500D);
 
 //        MID
         Tab tab = new Tab();
-
+        
         tab.setText("Nothing here");
         tabPane.getTabs().add(tab);
-
+        
         borderPane.setTop(menuBar);
         borderPane.setLeft(vBox);
         borderPane.setCenter(tabPane);
-
+        
         StackPane root = new StackPane();
         root.getChildren().addAll(borderPane);
 
-        Scene scene = new Scene(root, 1500, 750);
+        Scene scene = new Scene(root);
         primaryStage.setTitle("Quandl JAVA_FX");
         primaryStage.setScene(scene);
+        primaryStage.setMaximized(true);
         primaryStage.show();
-
+        
         Thread t = new Thread(new CreateWIKIDBTask(data));
         t.setDaemon(true);
         t.start();
-
+        
         table.setSkin(new DataSetTableViewSkin(table));
-
+        
         data.addListener(new ListChangeListener<DataSetFX>() {
             @Override
             public void onChanged(ListChangeListener.Change<? extends DataSetFX> c) {
-
+                
                 Platform.runLater(new Runnable() {
                     @Override
                     public void run() {
                         DataSetTableViewSkin ds = (DataSetTableViewSkin) table.getSkin();
                         ds.resizeColumnToFitContent(table.getColumns().get(1), Integer.MAX_VALUE);
                     }
-
+                    
                 });
             }
         });
-
+        
     }
-
+    
     public static void main(String[] args) {
         launch(args);
     }
-
+    
 }
